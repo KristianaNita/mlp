9690e56a9d8d0113d9638c3a95b5abe7b9caada2
Marina Potsi <s1958674@archlute.inf.ed.ac.uk>
2019-09-29 21:37:50 +0100

    Add code for lab 1

--- mlp/data_providers.py
+++ mlp/data_providers.py
@@ -133,11 +133,12 @@ class MNISTDataProvider(DataProvider):
         super(MNISTDataProvider, self).__init__(
             inputs, targets, batch_size, max_num_batches, shuffle_order, rng)
 
-    # def next(self):
-    #    """Returns next data batch or raises `StopIteration` if at end."""
-    #    inputs_batch, targets_batch = super(MNISTDataProvider, self).next()
-    #    return inputs_batch, self.to_one_of_k(targets_batch)
-    #
+    def next(self):
+       """Returns next data batch or raises `StopIteration` if at end."""
+       inputs_batch, targets_batch = super(MNISTDataProvider, self).next()
+#        print(targets_batch)
+       return inputs_batch, self.to_one_of_k(targets_batch)
+    
     def __next__(self):
         return self.next()
 
@@ -156,7 +157,17 @@ class MNISTDataProvider(DataProvider):
             to zero except for the column corresponding to the correct class
             which is equal to one.
         """
-        raise NotImplementedError()
+        encoded_array = []
+        
+        for target in int_targets:
+            enc_vector = np.zeros(10)
+            enc_vector[target] = 1
+            encoded_array.append(enc_vector)
+            
+#         print(int_targets.shape)
+        encoded_array = np.array(encoded_array)
+        return encoded_array
+#         raise NotImplementedError()
 
 
 class MetOfficeDataProvider(DataProvider):
@@ -188,11 +199,13 @@ class MetOfficeDataProvider(DataProvider):
             'Data file does not exist at expected path: ' + data_path
         )
         # load raw data from text file
-        # ...
+        data = np.loadtxt(data_path, skiprows=3)
+        data = data[:, 3:]
         # filter out all missing datapoints and flatten to a vector
-        # ...
+        data = data[data != -99.99]
+
         # normalise data to zero mean, unit standard deviation
-        # ...
+        normed = (data - data.mean(axis=0)) / data.std(axis=0)
         # convert from flat sequence to windowed data
         # ...
         # inputs are first (window_size - 1) entries in windows
